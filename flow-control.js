96dbf4cbf431cc64f1e7a2a54abc96a0521985b8
Hanna Lee <mani21na@gmail.com>
2019-08-12 13:37:14 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,19 @@
-function basicTeenager(age) {
+var age = 15;
 
+function basicTeenager(age) {
+  if(age>=13 && age<=19) {
+    console.log('You are a teenager!');
+  } else {
+            return undefined 
+  }
 }
 
 function teenager(age) {
-
+  if(age>=13 && age<=19) {
+    console.log('You are a teenager!')
+  } else {
+            console.log('You are not a teenager')
+  }
 }
 
 function ageChecker(age) {
